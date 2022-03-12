05747780f9dc99acb6eed772ca22e63705275fd4
krouspy <krouspyy@gmail.com>
2022-03-12 19:11:01 +0100

    modify accounts procedures: add middleware

--- src/modules/server/routers/accounts.ts
+++ src/modules/server/routers/accounts.ts
@@ -3,23 +3,30 @@ import { TRPCError } from '@trpc/server';
 import { createRouter } from '~/server/create-router';
 import { isAddress } from '~/modules/utils/web3';
 
+const inputSchema = z.object({ address: z.string().nonempty('accounts: address must not be empty').optional() });
+
 export const accountsRouter = createRouter()
+  .middleware(({ ctx, rawInput, next }) => {
+    const input = inputSchema.safeParse(rawInput);
+    if (!input.success) throw new TRPCError({ code: 'BAD_REQUEST' });
+    const { address } = input.data;
+    // address is defined on client-side when the wallet is connected so first render is undefined
+    if (!address) return next({ ctx });
+    if (!isAddress(address)) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'address must be an address',
+      });
+    }
+
+    return next({ ctx });
+  })
   .query('.nfts', {
-    input: z.object({
-      address: z.string().nonempty('accounts: address must not be empty').optional(),
-    }),
+    input: inputSchema,
     async resolve({ ctx, input }) {
       const { prisma } = ctx;
       const { address } = input;
-      if (!address) {
-        return [];
-      }
-      if (!isAddress(address)) {
-        throw new TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'address must be an address',
-        });
-      }
+      if (!address) return [];
       return prisma.contract.findMany({
         where: {
           tokens: {
@@ -39,21 +46,11 @@ export const accountsRouter = createRouter()
     },
   })
   .query('.deposited', {
-    input: z.object({
-      address: z.string().nonempty('accounts: address must not be empty').optional(),
-    }),
+    input: inputSchema,
     async resolve({ ctx, input }) {
       const { prisma } = ctx;
       const { address } = input;
-      if (!address) {
-        return [];
-      }
-      if (!isAddress(address)) {
-        throw new TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'address must be an address',
-        });
-      }
+      if (!address) return [];
       return prisma.token.findMany({ where: { depositedInVault: true, depositorAddress: address } });
     },
   });
